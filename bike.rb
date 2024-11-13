f35f3af2e0b4010693bcd79c9f55a13632bf318e
Ethan Price <priceeth@oregonstate.edu>
2024-11-12 16:34:08 -0800

    Refactored bike.rb to follow POODR Chapter 2 principles

--- bike.rb
+++ bike.rb
@@ -1,11 +1,10 @@
 # Bike
 
 class Bike
-
   STANDARD_WEIGHT = 200 # lbs
   MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_reader :id, :color, :price, :weight, :rented, :cargo_contents
 
   def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
     @id = id
@@ -16,16 +15,25 @@ class Bike
     @cargo_contents = []
   end
 
+
   def rent!
-    self.rented = true
+    @rented = true
+  end
+
+  def return!
+    @rented = false
   end
 
   def add_cargo(item)
-    self.cargo_contents << item
+    if cargo_full?
+      puts "Cannot add #{item}: pannier is full."
+    else
+      @cargo_contents << item
+    end
   end
 
   def remove_cargo(item)
-    self.cargo_contents.remove(item)
+    @cargo_contents.delete(item)
   end
 
   def pannier_capacity
@@ -33,7 +41,13 @@ class Bike
   end
 
   def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
+    pannier_capacity - @cargo_contents.size
   end
 
+  
+  private
+
+  def cargo_full?
+    @cargo_contents.size >= pannier_capacity
+  end
 end
